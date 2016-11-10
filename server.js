d60c760dc50d62b7d2aa5f9ee5432a409e7dd217
Vinicius Mussak <mussak@engsolutions.com.br>
2016-11-10 18:35:43 -0200

    a

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const 	express = require('express'),
 		load = require('express-load');
 var app = express();
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
 	console.log('Server running! :D');
 });
 
